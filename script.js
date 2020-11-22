0c501efddcdd499ae70c00204fbaf7a30c7881e5
averlye <averlye@github.com>
2020-11-22 13:37:07 +0100

    forecast JS
    change of icon and temperature for forecast

--- script.js
+++ script.js
@@ -28,7 +28,7 @@ function formatDate () {
 }
 
 function showTemperature(response) {
-
+  console.log(response.data);
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("h1");
   let iconElement = document.querySelector("#icon-temperature");
@@ -37,17 +37,44 @@ function showTemperature(response) {
   let descriptionElement = document.querySelector("#description");
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
-  iconElement.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+  iconElement.setAttribute("src", `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
   windElement.innerHTML = Math.round(response.data.wind.speed);
   humidityElement.innerHTML = response.data.main.humidity;
   descriptionElement.innerHTML = response.data.weather[0].description;
+  //FORECAST API
+  let lat = response.data.coord.lat;
+  let lon = response.data.coord.lon;
+  let apiKey = "3b0ffc0f73dbf8aed77c35d6e45972de";
+  apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=hourly,current,minutely,alerts&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(showForecast);
 }
+//FORECAST
+function showForecast(response) {
+  let temperatureElement1 = document.querySelector("#weather-forecast-1");
+  let temperatureElement2 = document.querySelector("#weather-forecast-2");
+  let temperatureElement3 = document.querySelector("#weather-forecast-3");
+  let temperatureElement4 = document.querySelector("#weather-forecast-4");
+  let iconForecastElement1 = document.querySelector("#icon-forecast-1");
+  let iconForecastElement2 = document.querySelector("#icon-forecast-2");
+  let iconForecastElement3 = document.querySelector("#icon-forecast-3");
+  let iconForecastElement4 = document.querySelector("#icon-forecast-4");
+  temperatureElement1.innerHTML = `${Math.round(response.data.daily[0].temp.day)}°`;
+  temperatureElement2.innerHTML = `${Math.round(response.data.daily[1].temp.day)}°`;
+  temperatureElement3.innerHTML = `${Math.round(response.data.daily[2].temp.day)}°`;
+  temperatureElement4.innerHTML = `${Math.round(response.data.daily[3].temp.day)}°`;
+  iconForecastElement1.setAttribute("src", `https://openweathermap.org/img/wn/${response.data.daily[0].weather[0].icon}.png`);
+  iconForecastElement2.setAttribute("src", `https://openweathermap.org/img/wn/${response.data.daily[1].weather[0].icon}.png`);
+  iconForecastElement3.setAttribute("src", `https://openweathermap.org/img/wn/${response.data.daily[2].weather[0].icon}.png`);
+  iconForecastElement4.setAttribute("src", `https://openweathermap.org/img/wn/${response.data.daily[3].weather[0].icon}.png`);
+}
+
 //API
 function search(city) {
   let apiKey = "3b0ffc0f73dbf8aed77c35d6e45972de";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
   axios.get(apiUrl).then(showTemperature);
+
 }
 //DEFAULT SEARCH
 search("Warsaw");
@@ -66,6 +93,9 @@ function showCurrentLocation(position) {
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
 
   axios.get(apiUrl).then(showTemperature);
+
+  apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=hourly,current,minutely,alerts&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(showForecast);
 }
 
 function showPosition() {
